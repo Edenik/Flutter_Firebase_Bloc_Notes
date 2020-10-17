5697fc85bfae69beeff6544e28735fff4618936b
Edenik <Edenik5@gmail.com>
2020-10-17 18:19:20 +0300

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/flutter_bloc_notes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_bloc_notes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Firebase Bloc Notes',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Hello world!',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+}
