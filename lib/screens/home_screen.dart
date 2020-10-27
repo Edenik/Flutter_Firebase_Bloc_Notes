74ace4157753f18f9f9b9f81a502692af605c28a
Edenik <Edenik5@gmail.com>
2020-10-27 11:18:27 +0200

    CustomScrollView - SliverAppBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,8 +7,41 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<AuthBloc, AuthState>(
       builder: (context, state) {
-        return Scaffold();
+        return Scaffold(
+          body: _buildBody(context, state),
+        );
       },
+    );
+  }
+
+  Stack _buildBody(BuildContext context, AuthState authState) {
+    return Stack(
+      children: [
+        CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              floating: true,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text('Youre Notes'),
+              ),
+              leading: IconButton(
+                icon: authState is Authenticated
+                    ? Icon(Icons.exit_to_app)
+                    : Icon(Icons.account_circle),
+                iconSize: 28.0,
+                onPressed: () => authState is Authenticated
+                    ? context.bloc<AuthBloc>().add(Logout())
+                    : print('go to login'),
+              ),
+              actions: <Widget>[
+                IconButton(
+                    icon: Icon(Icons.brightness_4),
+                    onPressed: () => print('change theme'))
+              ],
+            )
+          ],
+        )
+      ],
     );
   }
 }
